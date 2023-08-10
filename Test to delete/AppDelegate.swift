17cf9e1936200a1a63e0af3aa3bdf6aa59e5c02e
jerryseigle <mr.jerryseigle@gmail.com>
2023-08-10 16:19:31 -0400

    Initial Commit

---
+++ Test to delete/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Test to delete
+//
+//  Created by Jerry Seigle on 8/10/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Test to delete/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Test to delete
+//
+//  Created by Jerry Seigle on 8/10/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
